4bb5aab1d126058171001202e206f6ce356ef94b
Nathan van Beelen <nathan@vanbeelen.org>
2025-05-25 23:18:41 +0200

    Switch to Pelican as a SSG

--- theme/static/js/common.js
+++ theme/static/js/common.js
@@ -1,7 +1,11 @@
+// Include the navbar on the page.
+fetch('navbar.html')
+.then(response => response.text())
+.then(data => document.getElementById('navbar').innerHTML = data);
+
 // Hide the navbar after clicking on any of its elements.
 document.querySelector(".navbar").addEventListener('click', function() {
   document.querySelectorAll('.navbar .collapse').forEach(function (element) {
     new bootstrap.Collapse(element).hide();
   });
 });
-
